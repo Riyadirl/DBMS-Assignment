561b3142cbc0ca91253e34fc5231d5c26875c728
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-17 00:43:40 +0600

    .

--- details.php
+++ details.php
@@ -1,44 +1,69 @@
 <?php
-// Check if the id parameter is provided in the URL
-if (isset($_GET['id'])) {
-    // Get the id from the URL
-    $id = mysqli_real_escape_string($conn, $_GET['id']);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-    // Create SQL query to retrieve the pizza details
-    $sql = "SELECT * FROM pizzas WHERE id = $id";
+include('./db_connect.php');
 
-    // Execute the query and get the result
-    $result = mysqli_query($conn, $sql);
+if (isset($_POST['Delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
 
-    // Fetch the resulting row as an array
-    $pizza = mysqli_fetch_assoc($result);
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
 
-    // Free the result from memory
-    mysqli_free_result($result);
-
-    // Close the database connection
-    mysqli_close($conn);
-} else {
-    // Redirect to index.php if id parameter is not provided
-    header('Location: index.php');
+    // check delete
+    if (mysqli_query($conn, $sql)) {
+        header('Location: index.php');
+        exit();
+    } else {
+        echo 'Query error: ' . mysqli_error($conn);
+    }
 }
 
+// check GET request id parameter
+if (isset($_GET['id'])) {
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+
+    // make sql
+    $sql = "SELECT * FROM pizzas WHERE id = $id";
+
+    // get the query result
+    $result = mysqli_query($conn, $sql);
+
+    // fetch result in array format
+    $pizza = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+}
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <?php include('./header.php'); ?>
-<div class="container center grey-text">
+
+<div class="container center">
     <?php if ($pizza) : ?>
+
     <h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
     <p>Created by: <?php echo htmlspecialchars($pizza['email']); ?></p>
-    <p><?php echo date($pizza['created_at']); ?></p>
+    <p><?php echo date('F j, Y', strtotime($pizza['created_at'])); ?></p>
     <h5>Ingredients:</h5>
     <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+    <!-- Delete form -->
+    <form action="details.php" method="POST">
+        <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+        <input type="submit" name="Delete" value="Delete" class="deletebtn">
+    </form>
+
     <?php else : ?>
-    <h5>No pizza found.</h5>
+
+    <h5 class="error-heading">No such pizza exists!</h5>
+
     <?php endif; ?>
 </div>
+
 <?php include('./footer.php'); ?>
 
 </html>
--- index.php
+++ index.php
@@ -1,33 +1,66 @@
 <?php
-// connect to database
+
+//connect to database
+//$conn = mysqli_connect('localhost', 'ninja123', 'test123', 'ninja_pizza');
+
+//check connection
+//if (!$conn) {
+//echo 'connection error' . mysqli_connect_error();
+//}
+
+//connect db from config folder
+
 include('./db_connect.php');
 
-// write query for all pizzas
+
+
+//write query for all pizzas
 $sql = 'SELECT title, ingredients, id FROM pizzas';
 
-// make query and get result
+//make query and get result
 $result = mysqli_query($conn, $sql);
 
 // fetch the resulting rows as an array
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+
+//print_r($pizzas);
+
+
+
+
+
 // close connection
 mysqli_close($conn);
-?>
 
+//explode(',', $pizzas[0]['ingredients']);
+
+
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
+
 <?php include('./header.php'); ?>
 
 <h4 class="center grey-text">Pizzas!</h4>
 
 <div class="container">
     <div class="row">
+
         <?php foreach ($pizzas as $pizza) : ?>
+
         <div class="col s6 md3">
             <div class="card z-depth-0">
                 <img src="./pizza.png" class="pizza">
                 <div class="card-content center">
+
                     <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                     <ul>
                         <?php foreach (explode(',', $pizza['ingredients']) as $ing) : ?>
@@ -36,14 +69,30 @@ mysqli_close($conn);
                     </ul>
                 </div>
                 <div class="card-action right-align">
-                    <a href="details.php?id=<?php echo $pizza['id'] ?>" class="brand-text">More info</a>
+                    <a href="details.php?id=<?php echo $pizza['id'] ?>" class="brand-text">more info</a>
                 </div>
             </div>
         </div>
         <?php endforeach; ?>
+
+        <!-- <?php if (count($pizzas) >= 3) : ?>
+                <p>there are 2 or more pizzas</p>
+            <?php else : ?>
+                <p>there are less than 2 pizzas</p>
+            <?php endif; ?> -->
     </div>
 </div>
 
+
+
+
+
 <?php include('./footer.php'); ?>
+
+
+
+
+
+
 
 </html>
